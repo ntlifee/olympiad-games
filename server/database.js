396b0ce1a38ea312e9b6bc8d5bb6b2be019617ff
CsharpMaster37 <misha310303@gmail.com>
2024-09-26 22:41:38 +0300

    Подключил БД

--- server/database.js
+++ server/database.js
@@ -1,4 +1,12 @@
 const Sequelize = require('sequelize')
 
-
-module.exports
+module.exports = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        dialect: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT
+    }
+)
--- server/index.js
+++ server/index.js
@@ -2,13 +2,23 @@ require('dotenv').config()
 const express = require('express')
 const router = require('./routes/indexRouter.js')
 const cors = require('cors')
+const sequelize = require('./database.js')
 
 const PORT = process.env.PORT
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
 
-app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
+    } catch(e){
+        console.log(e)
+    }
+}
+start()
+
